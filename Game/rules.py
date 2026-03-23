234418649b5ee919fa844beb2c4e22e1828d2184
pereiragoncalo <pereira.goncalo@gmail.com>
2026-03-23 11:36:54 +0000

    Add files via upload

--- Game/rules.py
+++ Game/rules.py
@@ -1,1 +1,7 @@
+def shot_is_outside_small_box(x, y, small_box):
+    x1, y1, x2, y2 = small_box
+    return not (x1 <= x <= x2 and y1 <= y <= y2)
 
+
+def is_goal(ball_x, ball_y, goal_left, goal_right, goal_line_y):
+    return goal_left <= ball_x <= goal_right and ball_y <= goal_line_y
